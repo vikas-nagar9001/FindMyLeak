1bf2e6e55e3da9677d3ca6577f7f6c41e6c55c71
Vikas Nagar <vikasnagar9001@gmail.com>
2025-11-15 03:02:54 +0530

    up

--- backend/src/main/java/com/findmyleak/config/SecurityConfig.java
+++ backend/src/main/java/com/findmyleak/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:*", "http://127.0.0.1:*","https://findmyleak.render.com:*"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:*", "http://127.0.0.1:*","https://findmyleak.onrender.com:*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
